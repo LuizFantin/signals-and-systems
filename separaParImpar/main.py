de4d7addcea9e50c5b65d56c587432f99d858da8
Luiz Fantin <fantin_luiz@outlook.com>
2020-09-17 15:56:19 -0300

    Otimization

--- separaParImpar/main.py
+++ separaParImpar/main.py
@@ -1,15 +1,13 @@
 import numpy as np
 
 def main():
-    tam = 4
     signal = np.array([2,1,0,1,2])
     signalRev = signal[::-1]
-
     sp = []
     si = []
 
     x = 0
-    while x <= tam:
+    while x < signal.size:
         pass
         sp.append((1/2)*(signal[x]+signalRev[x]))
         si.append((1/2)*(signal[x]-signalRev[x]))
